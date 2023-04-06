0d97c23dec64428fb0c474dd764e31e471cf7814
CVrgas <usual389@gmail.com>
2023-04-06 17:49:25 -0400

    Added Booking form drop-of/pick-up option and min dates

---
+++ src/app/Models/car.ts
@@ -0,0 +1,10 @@
+export interface Car {
+    Model: string,
+    Mark: string,
+    Year: number,
+    Doors: number,
+    AC: boolean,
+    Transmission: string,
+    Fuel: string,
+    Price: number
+}
--- src/app/components/small-comp/booking-form/booking-form.component.ts
+++ src/app/components/small-comp/booking-form/booking-form.component.ts
@@ -1,10 +1,81 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Car } from 'src/app/Models/car';
 
 @Component({
   selector: 'app-booking-form',
   templateUrl: './booking-form.component.html',
   styleUrls: ['./booking-form.component.css']
 })
-export class BookingFormComponent {
-
+export class BookingFormComponent implements OnInit {
+  ngOnInit(): void {
+  }
+  available_cars: Car[] = [
+    {
+      Model: 'Chevrolet',
+      Mark: 'Silverado',
+      Year: 2012,
+      Doors: 4,
+      AC: true,
+      Transmission: 'Manual',
+      Fuel: 'Diesel',
+      Price: 0
+    },
+    {
+      Model: 'Ford',
+      Mark: 'F150',
+      Year: 2020,
+      Doors: 4,
+      AC: true,
+      Transmission: 'Automatic',
+      Fuel: 'Gasoline',
+      Price: 0
+    },
+    {
+      Model: 'Toyota',
+      Mark: 'RAV4',
+      Year: 2019,
+      Doors: 4,
+      AC: true,
+      Transmission: 'Manual',
+      Fuel: 'Diesel',
+      Price: 0
+    },
+    {
+      Model: 'Ram',
+      Mark: '1500',
+      Year: 2023,
+      Doors: 4,
+      AC: true,
+      Transmission: 'Automatic',
+      Fuel: 'Gasoline',
+      Price: 0
+    },
+    {
+      Model: 'Toyota',
+      Mark: 'Camry',
+      Year: 2024,
+      Doors: 4,
+      AC: true,
+      Transmission: 'Automatic',
+      Fuel: 'Hybrid',
+      Price: 0
+    }
+  ];
+  available_citys: string[] = [
+    'Santo Domingo', 'Barahona', 'Santiago', 'Puerto Plata', 'Monte Cristi', 'Punta Cana'
+  ]
+  today_date: Date = new Date;
+  drop_date: Date = new Date;
+  min_drop(){
+    const pickup_date = <HTMLInputElement>document.getElementById('Pick-up')
+    const Dropof_date = <HTMLInputElement>document.getElementById('Drop-of')
+    
+    console.log(pickup_date.value)
+    let _date: Date = new Date(pickup_date.value)
+    Dropof_date.setAttribute("min", 
+    `${_date.getFullYear()}
+    -${("0" + (_date.getMonth() + 1)).slice(-2)}
+    -${("0" + (_date.getDate() + 2)).slice(-2)}
+    `)
+  }
 }
